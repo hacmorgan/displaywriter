732c71c5b5799cd919bb51b3abdcca37ed9d0402
Hamish Morgan <ham430@gmail.com>
2022-01-17 19:21:17 +1100

    Successfully detect if a key is pressed!

---
+++ arduino_capsense/arduino_capsense.ino
@@ -0,0 +1,78 @@
+/**
+ * Detect whether a single key has been pressed on an IBM Beam Spring keyboard
+ *
+ * @author  Hamish Morgan
+ * @date    16/01/2021
+ * @license BSD
+ */
+
+
+// Clock output pin
+#ifdef __AVR_ATmega2560__
+  const byte CLOCKOUT = 11;  // Mega 2560
+#else
+  const byte CLOCKOUT = 9;   // Uno, Duemilanove, etc.
+#endif
+
+// Sense pins
+const byte COMPARATOR_OUT = 7;
+const byte CAP_OUT = 3;
+
+// Timing variables
+volatile unsigned long start_time = 0;
+volatile unsigned long end_time = 0;
+
+
+void setup_clockout()
+{
+  /**
+   * Configure the arduno to output the clock signal
+   */
+  // set up 8 MHz timer on CLOCKOUT (OC1A)
+  pinMode (CLOCKOUT, OUTPUT); 
+  // set up Timer 1
+  TCCR1A = bit (COM1A0);  // toggle OC1A on Compare Match
+  TCCR1B = bit (WGM12) | bit (CS10);   // CTC, no prescaling
+  OCR1A =  8;       // output every 256th cycle
+}
+
+
+void setup_sense_pin(byte sense_pin)
+{
+  /**
+   * Configure the sensing pin
+   */
+  pinMode(sense_pin, INPUT_PULLUP);
+  /* attachInterrupt(digitalPinToInterrupt(sense_pin), rise, RISING); */
+  /* attachInterrupt(digitalPinToInterrupt(sense_pin), fall, FALLING); */
+}
+
+
+void setup()
+{
+  setup_clockout();
+  setup_sense_pin(COMPARATOR_OUT);
+  Serial.begin(38400);
+}
+
+
+void loop()
+{
+  if (digitalRead(COMPARATOR_OUT) == HIGH) {
+    Serial.print(micros());
+    Serial.println("Key pressed!");
+  }
+}
+
+
+void rise()
+{
+  start_time = micros();
+}
+
+
+void fall()
+{
+  end_time = micros();
+  Serial.println(end_time - start_time);
+}
